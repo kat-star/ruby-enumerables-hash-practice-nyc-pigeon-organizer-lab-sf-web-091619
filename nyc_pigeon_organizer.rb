7e0f2c8e1a2d596697688240f7b30f1ad65edf18
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-14 04:11:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 def nyc_pigeon_organizer(data)
   #create a hash with the bird names as keys (map over values to get names in array)
-  #iterate over data hash (using reduce to create a new hash)
-  #the new keys are the names of the pigeons and the value will be its list of attributes()
-  #to get the names of pigeons
+  #iterate over data hash (using each to do nested loops to iterate thru the nested hash data)
+  #once I get to the array of names, push that attribute key into the new array as its value
   
+  hash_values = data.values[0].value
   
   
 end
