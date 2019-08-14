acd1feaa010d7c7ccfb694913c91b6d7acec4b92
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-14 04:06:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 def nyc_pigeon_organizer(data)
+  #create a hash with the bird names as keys (map over values to get names in array)
   #iterate over data hash (using reduce to create a new hash)
   #the new keys are the names of the pigeons and the value will be its list of attributes()
   #to get the names of pigeons
   
-  data.reduce({}) do |memo, (key, value)
+  
   
 end
