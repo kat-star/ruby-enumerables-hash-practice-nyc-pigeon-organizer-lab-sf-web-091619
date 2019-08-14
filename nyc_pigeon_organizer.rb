bbd8fe96a1fb91d9d2c7c062ac75f3789cf7242c
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-14 04:25:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,12 @@
 def nyc_pigeon_organizer(data)
-  #create a hash with the bird names as keys (map over values to get names in array)
   #iterate over data hash (using each to do nested loops to iterate thru the nested hash data)
-  #once I get to the array of names, push that attribute key into the new array as its value
+  #once I get to the array of names, set the name as the key (and check to see if it's already there, use ||=, and then push that attribute key into the new array as its value
   
-  hash_values = data.values[0].value
+  new_hash = Hash.new
+  
+  data.each do |attribute, values_h|
+  end
+    values_
   
   
 end
